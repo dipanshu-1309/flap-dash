be9ddce3168d4d7995fe483a14b5c5964c851a43
Dipanshu Rawat <dipanshurawat4@gmail.com>
2026-03-05 03:45:21 +0530

    adding gravitational effect to the bird

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import React, { useEffect } from "react";
 import { Canvas, useImage, Image} from "@shopify/react-native-skia";
 import { useWindowDimensions } from "react-native";
-import { Easing, useSharedValue, withTiming } from "react-native-reanimated";
+import { Easing, useFrameCallback, useSharedValue, withRepeat, withSequence, withTiming } from "react-native-reanimated";
+
+
+const GRAVITY = 10;
 
 const App = () => {
   const {width, height} = useWindowDimensions();
@@ -14,10 +17,28 @@ const App = () => {
   const base = useImage(require('./assets/sprites/base.png'))
 
   const x = useSharedValue(width - 50); //to update the animation value without rerender
+  const birdY = useSharedValue(height/2);
+  const birdYVelocity = useSharedValue(100);
+
+  useFrameCallback(({ timeSincePreviousFrame : dt })=>{
+    if (!dt){
+      return;
+    }
+    birdY.value = birdY.value + (birdYVelocity.value * dt)/1000;
+    birdYVelocity.value = birdYVelocity.value + (GRAVITY * dt)/1000 //to inc. velocity based on acc due to grav
+  });
+  //useFramCallback is called at every frame render
 
   useEffect(()=>{
-    x.value = withTiming(-200, { duration: 3000, easing: Easing.linear});//withTiming makes the movement of pipe slowly
-  },[])
+    x.value = withRepeat( withSequence (
+      withTiming(-200, { duration: 3000, easing: Easing.linear}),
+      withTiming(width ,{duration: 0})
+  ), -1 // -1 = repetition infinte num of times
+);//withTiming makes the movement of pipe slowly, withSequence runs animations one after another//withRepeat used for repition
+      
+  birdY.value= withTiming(height, {duration: 1000})
+
+}, []);
 
   const pipeOffset = 0; //moves pipes up and down
 
@@ -29,11 +50,11 @@ const App = () => {
       <Image image={pipeTop} x={x} y={pipeOffset-320} width={104} height={640}/>
       <Image image={pipeBottom} x={x} y={height - 320 + pipeOffset} width={104} height={640}/>
       {/*Base*/}
-      <Image image={base} x={-extraspaceBase} y={height - 75} width={width} height={150} fit={'cover'}/>
+      <Image image={base} x={0} y={height - 75} width={width} height={150} fit={'cover'}/>
 
       {/*Bird*/}
       <Image image={bird}
-      y={height/2 - 24} //the top left corner of the asset is considered the point of origin so we subtracted the pixel size according from x and y
+      y={birdY} //the top left corner of the asset is considered the point of origin so we subtracted the pixel size according from x and y
       x={width/4} height={48} width={64} />
     </Canvas>
   );
